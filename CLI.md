95b76d9a39e5f0e93741dbf86d7cc8b79d9bb3e7
user169155 <user169155@74-80-245-9.ic2net.net>
2020-03-26 14:06:35 -0700

    2020-03-26

--- CLI.md
+++
@@ -1,2 +0,0 @@
-gcc $(gnustep-config --objc-flags) -o hello hello.m $(gnustep-config --base-libs)
-./hello
---
+++ ObjC/ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjC
+//
+//  Created by user169155 on 3/26/20.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
